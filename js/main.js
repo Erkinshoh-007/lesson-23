b76d535ec043c9247409ec178853ececc5f0f20d
erkinshox <shaymurodoverkinshoh@gmail.com>
2024-07-06 18:59:51 +0500

    dkjfkdjf

--- js/main.js
+++ js/main.js
@@ -17,21 +17,30 @@ modeBtn.addEventListener("click", function () {
   }
   document.body.classList.toggle("dark");
 });
-//til
-document.getElementById("uzbekBtn").addEventListener("click", function () {
-  document.getElementById("title").textContent = "Salom Dunyo!";
-  document.getElementById("description").textContent =
-    "Bu sahifa tilini o'zgartiradi.";
+// navbar
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menu-toggleIconId");
+  const navMenuId = document.getElementById("nav-menuId");
+  const navBtnId = document.getElementById("nav-btnId");
+
+  console.log(menuToggle);
+  menuToggle.addEventListener("click", () => {
+    navMenuId.classList.toggle("active");
+    navBtnId.classList.toggle("activeBtn");
+  });
+});
+//navbar shirink
+
+window.addEventListener("scroll", function () {
+  shrink();
 });
 
-document.getElementById("russianBtn").addEventListener("click", function () {
-  document.getElementById("title").textContent = "Привет мир!";
-  document.getElementById("description").textContent =
-    "Эта страница изменяет язык.";
-});
+let navbar = document.getElementById("navbar");
 
-document.getElementById("englishBtn").addEventListener("click", function () {
-  document.getElementById("title").textContent = "Hello World!";
-  document.getElementById("description").textContent =
-    "This page changes the language.";
-});
+function shrink() {
+  if (scrollY > 10) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
